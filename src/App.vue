8e2db5acdcdb0f542dc2cbd6baed879694490eba
lyh <2953282238@qq.com>
2023-02-27 11:07:13 +0800

    right

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: right;
   color: #2c3e50;
   margin-top: 60px;
 }
